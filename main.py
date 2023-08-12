815c48068bb508cf8ffdbed8a45c628fb48935c3
Filipe André <flipeandre12@gmail.com>
2023-08-12 20:04:00 -0300

    feat: add ms

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ stringCifrada = "ilolsh"
 inicio = timeit.default_timer()
 print(f"Cifrado: {cifrar(stringParaCifrar, 3)}")
 print(f"Decifrado: {decifrar(stringCifrada, 3)}")
-
 fim = timeit.default_timer()
-print ('tempo de execução: %f' % (fim - inicio))
+tempo = fim - inicio
+print ('tempo de execução: %.3f ms' % ((tempo * 1000)))
