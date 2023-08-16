7779132ffa250c7649f9e5366ba0e4260b362060
Filipe André <flipeandre12@gmail.com>
2023-08-16 14:22:07 -0300

    feat: add brute force

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import timeit
 from functions.cython.funcaoCifrar import cifrar
 from functions.cython.funcaoDecifrar import decifrar
+from functions.cython.funcaoBruteForce import brute_force
 
 stringParaCifrar = "Filipe"
 stringCifrada = "ilolsh"
 
 inicio = timeit.default_timer()
-print(f"Cifrado: {cifrar(stringParaCifrar, 3)}")
-print(f"Decifrado: {decifrar(stringCifrada, 3)}")
+# print(f"Cifrado: {cifrar(stringParaCifrar, 3)}")
+# print(f"Decifrado: {decifrar(stringCifrada, 3)}")
+
+brute_force("ilolsh")
+
 fim = timeit.default_timer()
 tempo = fim - inicio
 print ('tempo de execução: %.3f ms' % ((tempo * 1000)))
